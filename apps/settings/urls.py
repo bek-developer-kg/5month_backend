aab48c2082c1bf1793f487c5ccebf0b99e1d9192
bek-developer-kg <bekkasymov365@icloud.com>
2025-01-25 22:07:07 +0600

    второй коммит

--- apps/settings/urls.py
+++ apps/settings/urls.py
@@ -1,9 +1,8 @@
-from django.urls import path
-from apps.settings.views import ProductCreateView, ProductListView, ProductDeleteView, ProductUpdateView
+from rest_framework.routers import DefaultRouter
+from apps.settings.views import ProductMixins
 
-urlpatterns = [
-    path('product/create/', ProductCreateView.as_view(), name = "product_create"),
-    path('product/list/', ProductListView.as_view(), name = "product_list"),
-    path('product/<int:pk>/delete/', ProductDeleteView.as_view(),name="product_delete"),
-    path('product/<int:pk>/update/', ProductUpdateView.as_view(),name="product_update"),
-]
+router = DefaultRouter()
+
+router.register(r'products', ProductMixins, basename='product')
+
+urlpatterns = router.urls
--- core/settings.py
+++ core/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -81,11 +82,15 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ.get('DB_NAME'),
+            'USER': os.environ.get('DB_USER'),
+            'PASSWORD': os.environ.get('DB_PASSWORD'),
+            'HOST': os.environ.get('DB_HOST'),
+            'PORT': os.environ.get('DB_PORT'),
+        }
     }
-}
 
 
 # Password validation
@@ -133,3 +138,43 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+JAZZMIN_SETTINGS = {
+    "site_title": "Geeks",  # Заголовок админ-панели
+    "site_header": "Geeks",  # Заголовок на экране входа
+    "site_brand": "Geeks",  # Бренд в верхней части админ-панели
+    "welcome_sign": "Добро пожаловать в Geeks",  # Приветственное сообщение
+    "site_title": "Geeks",  # Заголовок админ-панели
+    "site_header": "Geeks",  # Заголовок на экране входа
+    "site_brand": "Geeks",  # Бренд в верхней части админ-панели
+    "welcome_sign": "Добро пожаловать в Geeks",  # Приветственное сообщение
+    "search_model": ["auth.User", "blog.Post"],  # Модели, доступные для поиска
+    "topmenu_links": [
+        {"name": "Home", "url": "admin:index", "permissions": ["auth.view_user"]},
+    ],
+    "show_sidebar": True,
+    "changeform_format": "horizontal_tabs",
+    "header_classes": "navbar-dark bg-dark",  # Темный фон верхней части админ-панели
+    "header_color": "#000000",  # Черный цвет верхней части админ-панели
+    "dark_mode_theme": True,  # Включить темный режим
+    "show_language_chooser": True,  # Включить выбор языка в админ-панели
+    "custom_css": None,  # Путь к пользовательскому CSS-файлу (если нужен)
+    "show_ui_builder": True,  # Показать UI Builder
+    "menu": [
+        {
+            "app": "index",  # Имя вашего приложения Django
+            "name": "Основные параметры",  # Имя модели
+            "icon": "fa fa-cogs",  # Иконка для меню
+            "models": [
+                {
+                    "name": "Первая модель",  # Имя вашей модели
+                    "icon": "fa fa-cog",  # Иконка для модели
+                    "model": "index.Settings",  # Имя модели в формате "app_label.model_name"
+                },
+                # Добавьте другие модели, если необходимо
+            ],
+        },
+        # Добавьте другие приложения и модели, если необходимо
+    ],
+    
+}
